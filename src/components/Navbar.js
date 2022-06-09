190abb98160c64a9a6251bac07a2a50eb01cdc0f
M Zakiy Nurhasyim <zakiynurhasyim@gmail.com>
2022-06-09 15:53:19 +0700

    Navbar for mobile view and desktop

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+function Navbar() {
+  return (
+    <>
+      <div className="flex flex-row items-center pl-6 py-[17px] lg:py-[51] lg:pl-[213px]">
+        <div>
+          <img src="assets/png/Bitmap.png" alt="bitmap" className="w-[33px] h-[33px] mr-3" />
+        </div>
+        <div>
+          <div className="text-xs">Good Morning</div>
+          <div className="font-bold text-base">Fellas</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
---
+++ src/views/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Main from "../components/Main";
+
+function Home() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Main></Main>
+    </>
+  );
+}
+
+export default Home;
